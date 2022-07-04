ecd47c746e9214b99535ad38a86dc795a005c0db
Nil ANDREU <nil.andreu@veepee.com>
2022-07-04 13:08:38 +0200

    Feat: Function for Params

--- main-v2.go
+++ main-v2.go
@@ -4,14 +4,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func FunctionHandler(c *gin.Context) {
+func SimpleRequestHandler(c *gin.Context) {
 	c.JSON(200, gin.H {
-		"message" : "message",
+		"message" : "Hello world!",
+	})
+}
+
+func NameRequestHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(200, gin.H {
+		"message" : "Hello " + name,
 	})
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/", FunctionHandler)
+	router.GET("/", SimpleRequestHandler)
+	router.GET("/name/:name", NameRequestHandler)
 	router.Run(":8080")
 }
